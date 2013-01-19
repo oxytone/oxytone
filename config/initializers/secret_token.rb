c587ee581de4297582715584d6564593cc37732c
Tim Jüchter <t.juechter@oxytone.de>
2013-01-19 12:51:50 +0100

    Initital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oxytone::Application.config.secret_token = '242abb520860bb07650435a729fefb48736ad9dcbe14aaf27ef92d329a3893ec0d95fd465da4d064b2a1513e3c3327c4979df4ceb6a9c5f62830cbfbd44bb454'
