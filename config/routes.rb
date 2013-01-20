ba234864c7334ca9f7de18156cf864bae5940c2b
Tim Jüchter <t.juechter@oxytone.de>
2013-01-20 11:55:16 +0100

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Oxytone::Application.routes.draw do
+  resources :mircoposts
+
+
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
